4be4489459ed19d8005d40a4a8e3f53abeaf84bf
Jacob Lindahl <encody@users.noreply.github.com>
2023-09-14 20:00:49 +0900

    fix: build + deploy script cleanup

--- build.mjs
+++ build.mjs
@@ -13,14 +13,22 @@ const srcDocJs = srcDocJsBuildResult.outputFiles[0].text;
 
 const srcDoc = `<html><body><script>${srcDocJs}</script></body></html>`;
 
+const outdir =
+  process.env.NODE_ENV === 'production' ? 'build/prod/src' : 'build/dev/src';
+
+const entryPoints =
+  process.env.NODE_ENV === 'production'
+    ? ['src/CodeGenerator.jsx']
+    : ['src/CodeGenerator.jsx', 'src/Test.jsx'];
+
 await esbuild.build({
-  entryPoints: ['src/CodeGenerator.jsx', 'src/Test.jsx'],
+  entryPoints,
   platform: 'browser',
   target: 'es2017',
   jsx: 'preserve',
   define: {
     SRC_DOC: JSON.stringify(srcDoc),
   },
-  outdir: 'build',
+  outdir,
   outExtension: { '.js': '.jsx' },
 });
